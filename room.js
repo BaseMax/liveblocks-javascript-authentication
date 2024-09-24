8d6ac536dfc61c8cb18d16567d94b06a28d019d1
Max Base <MaxBaseCode@gmail.com>
2024-09-24 18:54:31 +0330

    update

--- room.js
+++ room.js
@@ -2,7 +2,7 @@ import { createClient } from "@liveblocks/client";
 
 const client = createClient({
   publicApiKey: "pk_dev_4hBTxoM5Vkir7gRMcIEP99BTDpqay7jjxcG9ELGa4fomzhWfbzkWgOh4qbU4Rhup",
-  authEndpoint: "/auth.php",
+  authEndpoint: "http://localhost/auth.php",
 });
 
 export const { room, leave } = client.enterRoom("my-room");
